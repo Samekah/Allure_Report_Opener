0567148c0fca253baefc95569698092bfee4c66b
Samekah <Lucidareverie@outlook.com>
2024-11-12 06:56:26 +0000

    completed todos: update choices to allow number options - quicker add if statement that is not case sensitive and moved both to separate methods

--- src/ReportSettings.java
+++ src/ReportSettings.java
@@ -6,8 +6,7 @@ public class ReportSettings {
 	//todo: Store default location somewhere and load it if not first time using application
 	//todo: Give user the option to change default location
 	//Todo: add method to close terminal window
-	//Todo: update choices to allow number options - quicker
-	//Todo: add if statement that is not case sensitive
+	//Todo: Commenting
 
 	private final Unzipper uz = new Unzipper();
 
@@ -19,16 +18,13 @@ public class ReportSettings {
 		String newDirectoryName = "";
 		String currentTime = "";
 
+		//todo: add check to see if it is a valid directory by 12/11
 		System.out.println("please enter the directory of the reports zip folder:");
-
 //		System.out.println(defaultDirectory + File.separator + "Allure Reports");
 		zipFileLocation = input.nextLine();
 
-		System.out.println("\nOut of the following, please select your chosen tenant:\n-> BQUK\n-> TPUK\n-> BQIE\n-> CASTOFR\n-> CASTOPL");
-		tenant = input.nextLine();
-
-		System.out.println("\nOut of the following, please select your chosen test stage:\n-> dev\n-> ci\n-> stage");
-		testStage = input.nextLine();
+		tenant = askForTenant(input);
+		testStage = askForTestStage(input);
 
 		currentTime = String.valueOf(System.currentTimeMillis());
 		newDirectoryName = tenant + "-" + testStage + "_" + currentTime;
@@ -37,8 +33,6 @@ public class ReportSettings {
 		//Todo: Move to separate method and complete opening process of report
 		//Todo: add initial check to see if a wt is open, if so add tab if not new window
 		//Todo: Check if processbuilder works for mac
-
-
 		try {
 			openReport(zipFileLocation,newDirectoryName);
 //			new ProcessBuilder("cmd.exe", "/c", "start cmd.exe").start().waitFor();  // Wait for the process to finish
@@ -51,9 +45,116 @@ public class ReportSettings {
 //		"C:\Users\howars19\OneDrive - Kingfisher PLC\Documents 1\Payments team\Pipeline reports\BQIE-dev_250924_2\kf-ra-gprs-tests\target\site\allure-maven-plugin"
 	}
 
+	public String askForTenant(Scanner i){
+
+		String tenant = "";
+		boolean isNumber = false;
+		boolean response = true;
+
+		while(response){
+			System.out.println("\nOut of the following, please select your chosen tenant:\n1) BQUK\n2) TPUK\n3) BQIE\n4) CASTOFR\n5) CASTOPL");
+			tenant = i.nextLine();
+
+			isNumber = isNumber(tenant);
+
+			if(isNumber){
+				switch(tenant){
+					case "1":
+						tenant = "BQUK";
+						response = false;
+						break;
+					case "2":
+						tenant = "TPUK";
+						response = false;
+						break;
+					case "3":
+						tenant = "BQIE";
+						response = false;
+						break;
+					case "4":
+						tenant = "CASTOFR";
+						response = false;
+						break;
+					case "5":
+						tenant = "CASTOPL";
+						response = false;
+						break;
+					default:
+						System.out.println("Please enter a valid response");
+						break;
+				}
+			}
+			else{
+				if(tenant.equalsIgnoreCase("BQUK") || tenant.equalsIgnoreCase("TPUK") || tenant.equalsIgnoreCase("BQIE") || tenant.equalsIgnoreCase("CASTOFR") || tenant.equalsIgnoreCase("CASTOPL")){
+					tenant = tenant.toUpperCase();
+					response = false;
+				}
+				else{
+					System.out.println("Please enter a valid response");
+				}
+			}
+		}
+
+		return tenant;
+	}
+
+	public String askForTestStage(Scanner i){
+
+		String testStage = "";
+		boolean isNumber = false;
+		boolean response = true;
+
+		while(response){
+			System.out.println("\nOut of the following, please select your chosen test stage:\n1) DEV\n2) CI\n3) STAGE");
+			testStage = i.nextLine();
+
+			isNumber = isNumber(testStage);
+
+			if(isNumber){
+				switch(testStage){
+					case "1":
+						testStage = "DEV";
+						response = false;
+						break;
+					case "2":
+						testStage = "CI";
+						response = false;
+						break;
+					case "3":
+						testStage = "STAGE";
+						response = false;
+						break;
+					default:
+						System.out.println("Please enter a valid response");
+						break;
+				}
+			}
+			else{
+				if(testStage.equalsIgnoreCase("DEV") || testStage.equalsIgnoreCase("CI") || testStage.equalsIgnoreCase("STAGE")){
+					testStage = testStage.toUpperCase();
+					response = false;
+				}
+				else{
+					System.out.println("Please enter a valid response");
+				}
+			}
+		}
+
+		return testStage;
+	}
+
 	private void openReport(String zipFilePath,String directoryName) throws IOException, InterruptedException{
 		uz.unzipFile(zipFilePath, uz.getDefaultDirectory(), directoryName);
 		new ProcessBuilder("cmd.exe", "/c", "start cmd.exe").start().waitFor();
 //		new ProcessBuilder("wt","-p", "Command Prompt","/c start cmd.exe" ).start().waitFor();
+	}
+
+	private boolean isNumber(String n){
+		try{
+			Integer.parseInt(n);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
 	}
 }
