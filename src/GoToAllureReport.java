64367b7dce52f44ab8728ad45ef115aee8eb62ed
Samekah <Lucidareverie@outlook.com>
2024-11-12 06:52:56 +0000

    Updated response option in GoToAllureReport

--- src/GoToAllureReport.java
+++ src/GoToAllureReport.java
@@ -9,6 +9,7 @@ public class GoToAllureReport {
 
 		String response = "";
 		boolean moreReports = true;
+		boolean keepReportsOpen = true;
 		Scanner input = new Scanner(System.in);
 		ReportSettings rs = new ReportSettings();
 
@@ -23,29 +24,28 @@ public class GoToAllureReport {
 				rs.allureReportSetup(input);
 			}
 			else if(response.equalsIgnoreCase("no") || response.equalsIgnoreCase("n")){
-				System.out.println("would you like to keep the Allure report server running? [yes/Y] or [no/N]");
-				response = input.nextLine();
+				while(keepReportsOpen) {
+					System.out.println("would you like to keep the Allure report server running? [yes/Y] or [no/N]");
+					response = input.nextLine();
 
-				if(response.equalsIgnoreCase("yes") || response.equalsIgnoreCase("y")){
-					rs.allureReportSetup(input);
-					moreReports = false;
-				}
-				else if(response.equalsIgnoreCase("no") || response.equalsIgnoreCase("n")){
-					try {
-						new ProcessBuilder("taskkill", "/F", "/IM", "cmd.exe").start().waitFor();
-						System.out.println("thank you for using this service!");
-						input.close();
+					if (response.equalsIgnoreCase("yes") || response.equalsIgnoreCase("y")) {
 						moreReports = false;
-					}catch (InterruptedException | IOException e) {
-						throw new RuntimeException(e);
+						keepReportsOpen = false;
+						input.close();
+					} else if (response.equalsIgnoreCase("no") || response.equalsIgnoreCase("n")) {
+						try {
+							new ProcessBuilder("taskkill", "/F", "/IM", "cmd.exe").start().waitFor();
+							input.close();
+							moreReports = false;
+							keepReportsOpen = false;
+						} catch (InterruptedException | IOException e) {
+							throw new RuntimeException(e);
+						}
+					} else {
+						System.out.println("Please enter a valid response");
 					}
 				}
-				else{
-					System.out.println("Please enter a valid response");
-				}
-
 				System.out.println("thank you for using this service!");
-				input.close();
 			}
 			else{
 				System.out.println("Please enter a valid response");
