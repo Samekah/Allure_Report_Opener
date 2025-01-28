2d60202aa51817251a301780285844c7c3d3bb5e
Samekah Howard <samekah.howard@kingfisher.com>
2025-01-28 17:43:29 +0000

    simplified main and added a main menu

--- src/GoToAllureReport.java
+++ src/GoToAllureReport.java
@@ -4,51 +4,33 @@ import java.util.Scanner;
 public class GoToAllureReport {
 
 	public static void main(String[]args){
-		// check which OS is being used
+		//todo: check which OS is being used
+		//todo: check default directory
 		String response;
-		boolean moreReports = true;
-		boolean keepReportsOpen = true;
+		boolean mainMenu = true;
+
 		Scanner input = new Scanner(System.in);
 		ReportSettings rs = new ReportSettings();
+		AppOptions ao = new AppOptions();
 
 		System.out.print("Welcome to this test program, it will do the following things\n1) Take in a zip folder\n2) Create a directory for an unzipped file\n3) Unzip the folder\n4) Save in directory\n5) open file\n\n");
-		rs.allureReportSetup(input);
 
-		while(moreReports){
-			System.out.println("Would you like to open another report? [yes/Y] or [no/N]");
+		while(mainMenu) {
+			System.out.println("======= Menu =======");
+			System.out.println("Please select one of the following options [1 / 2]:");
+			System.out.println("1) Open a report\n2) change output directory\n3) Exit\n");
+
 			response = input.nextLine();
 
-			if(response.equalsIgnoreCase("yes") || response.equalsIgnoreCase("y")){
-				rs.allureReportSetup(input);
-			}
-			else if(response.equalsIgnoreCase("no") || response.equalsIgnoreCase("n")){
-				while(keepReportsOpen) {
-					System.out.println("would you like to keep the Allure report server running? [yes/Y] or [no/N]");
-					response = input.nextLine();
-
-					if (response.equalsIgnoreCase("yes") || response.equalsIgnoreCase("y")) {
-						moreReports = false;
-						keepReportsOpen = false;
-						input.close();
-					} else if (response.equalsIgnoreCase("no") || response.equalsIgnoreCase("n")) {
-						try {
-							new ProcessBuilder("taskkill", "/F", "/IM", "WindowsTerminal.exe").start().waitFor();
-							input.close();
-							moreReports = false;
-							keepReportsOpen = false;
-						} catch (InterruptedException | IOException e) {
-							throw new RuntimeException(e);
-						}
-					} else {
-						System.out.println("Please enter a valid response");
-					}
-				}
-				System.out.println("thank you for using this service!");
-			}
-			else{
+			if (response.equals("1")) {
+				rs.reportMenu(input);
+			} else if (response.equals("2")) {
+//				ao;
+			} else if (response.equals("3") || response.equalsIgnoreCase("exit")) {
+				mainMenu = false;
+			} else {
 				System.out.println("Please enter a valid response");
 			}
-
 		}
 
 	}
