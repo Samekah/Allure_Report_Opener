8d113df3f04e931677c1780b341922ea0757a53c
Samekah Howard <samekah.howard@kingfisher.com>
2025-03-13 09:53:05 +0000

    updated test scripts - ADDED SEPARATE FILE CONFIG PATH

--- src/com/lucidstudios/AppOptions.java
+++ src/com/lucidstudios/AppOptions.java
@@ -12,7 +12,8 @@ import java.util.Properties;
 import java.util.Scanner;
 
 public class AppOptions {
-    private final String APPCONFIGPATH = Thread.currentThread().getContextClassLoader().getResource("com/lucidstudios/config.properties").getPath().replace("%20", " ").substring(1);
+    private final String APPCONFIGPATH = Thread.currentThread().getContextClassLoader().getResource("com/lucidstudios/config.properties").getPath().replace("/", "\\").replace("%20", " ").substring(1);
+    private final String APPLEAPPCONFIGPATH = Thread.currentThread().getContextClassLoader().getResource("com/lucidstudios/config.properties").getPath().replace("%20", " ").substring(1);
     private final String OPERATING_SYSTEM = System.getProperty("os.name");
     private String defaultOutputDirectory = new JFileChooser().getFileSystemView().getDefaultDirectory().toString() + File.separator + "Allure Reports";
     Properties appProps = new Properties();
@@ -22,7 +23,13 @@ public class AppOptions {
     public void checkFirstRun(){
 
         try {
-		    appProps.load(new FileInputStream(APPCONFIGPATH));
+            if(OPERATING_SYSTEM.toLowerCase().contains("mac")){
+                appProps.load(new FileInputStream(APPLEAPPCONFIGPATH));
+            }
+            else{
+                appProps.load(new FileInputStream(APPCONFIGPATH));
+            }
+
 	    } catch (IOException e) {
 		    throw new RuntimeException(e);
 	    }
