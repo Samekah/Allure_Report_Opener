d8030b588bd7471bd755cc5d847f0ab1a680dd0c
Samekah Howard <samekah.howard@kingfisher.com>
2025-03-13 09:41:15 +0000

    updated test scripts - removed replace for /

--- src/com/lucidstudios/AppOptions.java
+++ src/com/lucidstudios/AppOptions.java
@@ -12,7 +12,7 @@ import java.util.Properties;
 import java.util.Scanner;
 
 public class AppOptions {
-    private final String APPCONFIGPATH = Thread.currentThread().getContextClassLoader().getResource("com/lucidstudios/config.properties").getPath().replace("/", "\\").replace("%20", " ").substring(1);
+    private final String APPCONFIGPATH = Thread.currentThread().getContextClassLoader().getResource("com/lucidstudios/config.properties").getPath().replace("%20", " ").substring(1);
     private final String OPERATING_SYSTEM = System.getProperty("os.name");
     private String defaultOutputDirectory = new JFileChooser().getFileSystemView().getDefaultDirectory().toString() + File.separator + "Allure Reports";
     Properties appProps = new Properties();
